05018fd382154dc97a4bc70a01f0be534685203f
Alexandru Rotariu <alexandru.rotariu@emanuel.ro>
2021-03-16 17:15:33 +0200

    Add project files.

---
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcMovie.Models;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+
+        private static List<Movie> movies = new List<Movie>()
+        {
+            new Movie() { Id = Guid.NewGuid(), Title = "The Hunger Games", Genre = "Dystopian", Price = 15 },
+            new Movie() { Id = Guid.NewGuid(), Title = "The Sound of Music", Genre = "Musical", Price = 20 },
+            new Movie() { Id = Guid.NewGuid(), Title = "Lord of the Rings", Genre = "Adventure", Price = 24 },
+            new Movie() { Id = Guid.NewGuid(), Title = "City of Angels", Genre = "Romance", Price = 8 },
+            new Movie() { Id = Guid.NewGuid(), Title = "About Time", Genre = "Romance", Price = 19 }
+        };
+
+        [HttpGet]
+        public Movie[] Get()
+        {
+            return movies.ToArray();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Movie movie)
+        {
+            if (movie.Id == Guid.Empty)
+                movie.Id = Guid.NewGuid();
+
+            movies.Add(movie);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Movie movie)
+        {
+            Movie currentMovie = movies.FirstOrDefault(m => m.Id == movie.Id);
+            currentMovie.Title = movie.Title;
+            currentMovie.Genre = movie.Genre;
+            currentMovie.Price = movie.Price;
+        }
+
+        [HttpDelete]
+        public void Delete(Guid id)
+        {
+            movies.RemoveAll(movie => movie.Id == id);
+        }
+    }
+}
---
+++ MoviesAPI/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
